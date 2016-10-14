9bec32ab4fad16c62b75818a9f63766b8590337c
prashast1310 <prashast.vaidya@gmail.com>
2016-10-14 19:50:39 -0400

    Made the project with Android Studio

---
+++ app/src/main/java/edu/purdue/vaidya1/desiheadsup/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.purdue.vaidya1.desiheadsup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
